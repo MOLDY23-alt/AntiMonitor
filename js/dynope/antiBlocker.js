5fbf895682a96ea73f50ca044735dd1e2d5bfa4b
Noah <patchy319@gmail.com>
2019-10-31 16:30:05 -0400

    Added Icon Setting for setBlocked() and setUnBlocked() [UNTESTED]

--- js/dynope/antiBlocker.js
+++ js/dynope/antiBlocker.js
@@ -10,12 +10,24 @@ var antiBlocker = {
   setBlocked : function() {
     if(antiBlocker.blocked === false) {
       antiBlocker.blocked === true;
+      chrome.browserAction.setIcon({
+        path: {
+            "19": "images/icon19.png",
+            "38": "images/icon38.png"
+        }
+    });
     } 
   }
 
   setUnblocked : function() {
     if(!antiBlocker.blocked === false) {
       antiBlocker.blocked === false;
+    chrome.browserAction.setIcon({
+        path: {
+            "19": "images/custom_icon19.png",
+            "38": "images/custom_icon38.png"
+        }
+    });
     }
   }
 }
